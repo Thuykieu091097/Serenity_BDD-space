743e2ea500a5a3f5e42fb00f51a42f1cfbcec48c
Trần Thị Thuỷ Kiều <48143850+Thuykieu091097@users.noreply.github.com>
2019-05-07 10:15:43 +0700

    Delete HomePage.java

--- src/test/java/vn/company/auto/pages/HomePage.java
+++
@@ -1,8 +0,0 @@
-package vn.company.auto.pages;
-
-public class HomePage {
-	public static String  HOME_BUTTON ="xpath=//img[@alt='ThuyKieu Store']";
-	public static String LOGIN_HOME_BUTTON="xpath=//a[contains(text(),'Đăng nhập')]";
-	public static String REGISTER_HOME_BUTTON="xpath=//a[contains(text(),'Đăng ký')]";
-
-}
